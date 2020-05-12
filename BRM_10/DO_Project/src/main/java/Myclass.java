66d53f31435f2455304b0a9ba9c262214e5454c3
AMAN TYAGI <500069023@stu.upes.ac.in>
2020-05-12 16:46:21 +0530

    Experiment 10

---
+++ BRM_10/DO_Project/src/main/java/Myclass.java
@@ -0,0 +1,9 @@
+
+public class Myclass {
+
+	public static void main(String[] args) {
+		System.out.print("Myclass.java");
+
+	}
+
+}
---
+++ BRM_10/DO_Project/src/main/java/Myclass2.java
@@ -0,0 +1,8 @@
+
+public class Myclass2 {
+	public static void main(String[] args) {
+		System.out.print("Second class created by Prashasti");
+
+	}
+
+}
